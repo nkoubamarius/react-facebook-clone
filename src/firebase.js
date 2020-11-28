12196c09a607b013a0d8e99bc843639b9bee02b3
nkoubamarius <nkoubamarius@gmail.com>
2020-11-28 02:11:10 +0100

    Creation de la section Header

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// eslint-disable-next-line no-unused-vars
+const firebaseConfig = {
+    apiKey: "AIzaSyAB-EaJVUh2neVu7O5LAheez5UefxdirI8",
+    authDomain: "facebook-clone-aa186.firebaseapp.com",
+    databaseURL: "https://facebook-clone-aa186.firebaseio.com",
+    projectId: "facebook-clone-aa186",
+    storageBucket: "facebook-clone-aa186.appspot.com",
+    messagingSenderId: "833482561072",
+    appId: "1:833482561072:web:45b8e811c3edab0d867ff5",
+    measurementId: "G-SFTTDFZXES"
+  };
